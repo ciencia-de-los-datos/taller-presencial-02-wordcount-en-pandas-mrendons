195d35aead01f070bf0c2ab220930e5d9279adbe
mrendons <mrendons@unal.edu.co>
2024-03-01 19:55:50 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -5,28 +5,66 @@ import glob
 import pandas as pd
 
 
-def load_input(input_directory):
+def load_input(input_directory): #leer los archivos y volverlos a un solo dataframe
     """Load text files in 'input_directory/'"""
     #
     # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
     # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
     # entrada en el DataFrame.
     #
+    filenames = glob.glob(f"{input_directory}/*.txt")
+    
+    #dataframes = []
+    #for filename in filenames:
+    #dataframes.append( df = pd.read_csv(filenames[0], sep="\t", header=None, names=["text"]))
+    #df = pd.read_csv(filenames[0], sep="\t", header=None, names=["text"])#lee el archivo, lo convierte en un dataframe, heaber para el nombre de las columnas
+    
+    dataframes = [
+    pd.read_csv(filename, sep="\t", header=None, names=["text"])
+    for filename in filenames   
+    ]
+    
+    concatenated_df = pd.concat(dataframes, ignore_index=True)
+    return concatenated_df
 
-
-def clean_text(dataframe):
+            
+def clean_text(dataframe): #limpia los datos
     """Text cleaning"""
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
-
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.lower() #cambia mayusculas por minusculas
+    dataframe["text"] = dataframe["text"].str.replace(".", "")
+    dataframe["text"] = dataframe["text"].str.replace(",", "")
+    return dataframe
+    
 
 def count_words(dataframe):
     """Word count"""
+    
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.split()#truco: se parte por palabras, cada fila de la tabla en una lista de strings
+    dataframe = dataframe.explode("text")
+    dataframe["count"] = 1
+    dataframe = dataframe.groupby("text").agg({"count": "sum"})
+    return dataframe 
+
+def count_words_(dataframe):
+    """Word count"""
+    
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.split()#truco: se parte por palabras, cada fila de la tabla en una lista de strings
+    dataframe = dataframe.explode("text")
+    dataframe = dataframe["text"].value_counts()
+    
+    return dataframe
+
 
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
+    dataframe.to_csv(output_filename, sep="\t", index=True, header=False)
 
 
 #
@@ -34,6 +72,11 @@ def save_output(dataframe, output_filename):
 #
 def run(input_directory, output_filename):
     """Call all functions."""
+    df = load_input(input_directory)
+    df = clean_text(df)
+    df = count_words_(df)
+    save_output(df, output_filename)
+
 
 
 if __name__ == "__main__":
